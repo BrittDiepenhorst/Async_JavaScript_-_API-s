ed6a9b3f870de5eb59677aa8e474d8ea6aa99659
Britt Diepenhorst <brittdiepenhorst@hotmail.com>
2021-05-21 17:59:51 +0200

    Asyc Javascript

---
+++ Opdracht_Callbacks/script.js
@@ -0,0 +1,13 @@
+function huiswerkMaken(vak, callback) {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+    setTimeout(function () { }, 2000);
+    callback();
+};
+
+huiswerkMaken(`wiskunde`, klaarMetHuiswerk());
+
+function klaarMetHuiswerk() {
+    console.log(`Kijk Paps/Mams, ik ben klaar met mijn huiswerk!`);
+};
+
+huiswerkMaken(`wiskunde`, klaarMetHuiswerk());
